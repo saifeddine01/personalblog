66a1d949f031ee65d0e93a807f5b351b6187202d
saifeddine011 <saifeddine.sassi@protonmail.com>
2025-06-14 13:05:48 +0100

    update index page in order to test pipeline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MatButtonModule,MatInputModule],
+  imports: [RouterOutlet,MatButtonModule,MatInputModule,MatSlideToggleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
